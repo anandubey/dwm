38b19f1702db4ea117711b25ae01ff4210fee71d
Anand Kumar Dubey <dubey.anandkr@gmail.com>
2022-04-02 18:02:12 +0530

    Basic setup

--- config.def.h
+++ config.def.h
@@ -5,7 +5,7 @@ static const unsigned int borderpx  = 1;        /* border pixel of windows */
 static const unsigned int snap      = 32;       /* snap pixel */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
-static const char *fonts[]          = { "monospace:size=10" };
+static const char *fonts[]          = { "JetBrainsMono Nerd Font:size=10", "Material Icons:size=14" };
 static const char dmenufont[]       = "monospace:size=10";
 static const char col_gray1[]       = "#222222";
 static const char col_gray2[]       = "#444444";
@@ -34,7 +34,7 @@ static const Rule rules[] = {
 /* layout(s) */
 static const float mfact     = 0.55; /* factor of master area size [0.05..0.95] */
 static const int nmaster     = 1;    /* number of clients in master area */
-static const int resizehints = 1;    /* 1 means respect size hints in tiled resizals */
+static const int resizehints = 0;    /* 1 means respect size hints in tiled resizals */
 static const int lockfullscreen = 1; /* 1 will force focus on the fullscreen window */
 
 static const Layout layouts[] = {
@@ -46,6 +46,7 @@ static const Layout layouts[] = {
 
 /* key definitions */
 #define MODKEY Mod1Mask
+#define ALTKEY Mod4Mask
 #define TAGKEYS(KEY,TAG) \
 	{ MODKEY,                       KEY,      view,           {.ui = 1 << TAG} }, \
 	{ MODKEY|ControlMask,           KEY,      toggleview,     {.ui = 1 << TAG} }, \
@@ -58,22 +59,54 @@ static const Layout layouts[] = {
 /* commands */
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
 static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
-static const char *termcmd[]  = { "st", NULL };
+static const char *termcmd[]  = { "alacritty", NULL };
+static const char *rofi[] = { "rofi", "-show", "drun", NULL};
+
+/* Hardware keys for volume and brightness */
+#include <X11/XF86keysym.h>
+static const char *mutevol[] 			= { "pamixer", "--toggle-mute",  NULL };
+static const char *upvol[]   			= { "pamixer", "-ui", "5",  	NULL };
+static const char *downvol[] 			= { "pamixer", "-ud", "4",    	NULL };
+static const char *upbl[] 			= { "brightnessctl", "set", "+5%", NULL };
+static const char *downbl[] 			= {"brightnessctl", "set", "5-%", NULL };
+
+/* Screenshot */
+static const char *shotnow[]  			= { "takeshot", "--now", NULL };
+static const char *shotin5[]  			= { "takeshot", "--in5", NULL };
+static const char *shotin10[]  			= { "takeshot", "--in10", NULL };
+static const char *shotwin[]  			= { "takeshot", "--win", NULL };
+static const char *shotarea[]  			= { "takeshot", "--area", NULL };
 
 static Key keys[] = {
 	/* modifier                     key        function        argument */
-	{ MODKEY,                       XK_p,      spawn,          {.v = dmenucmd } },
-	{ MODKEY|ShiftMask,             XK_Return, spawn,          {.v = termcmd } },
-	{ MODKEY,                       XK_b,      togglebar,      {0} },
+
+	// Hardware Keys -----------
+	{ 0, 						XF86XK_AudioMute, 			spawn, {.v = mutevol } },
+	{ 0, 						XF86XK_AudioLowerVolume, 	spawn, {.v = downvol } },
+	{ 0, 						XF86XK_AudioRaiseVolume, 	spawn, {.v = upvol   } },
+	{ 0, 						XF86XK_MonBrightnessUp, 	spawn, {.v = upbl   } },
+	{ 0, 						XF86XK_MonBrightnessDown, 	spawn, {.v = downbl   } },
+
+	// Print Keys -----------
+	{ 0, 						XK_Print, 					spawn, {.v = shotnow } },
+	{ ALTKEY, 					XK_Print, 					spawn, {.v = shotin5 } },
+	{ ShiftMask, 				XK_Print, 					spawn, {.v = shotin10 } },
+	{ ControlMask, 				XK_Print, 					spawn, {.v = shotwin } },
+	{ ALTKEY|ControlMask, 		XK_Print, 					spawn, {.v = shotarea } },
+
+
+	{ MODKEY,                       XK_p,      spawn,          {.v = rofi} },
+	{ MODKEY,             XK_Return, spawn,          {.v = termcmd } },
+	{ MODKEY|ControlMask,                       XK_b,      togglebar,      {0} },
 	{ MODKEY,                       XK_j,      focusstack,     {.i = +1 } },
 	{ MODKEY,                       XK_k,      focusstack,     {.i = -1 } },
-	{ MODKEY,                       XK_i,      incnmaster,     {.i = +1 } },
-	{ MODKEY,                       XK_d,      incnmaster,     {.i = -1 } },
+	{ MODKEY|ControlMask,                       XK_i,      incnmaster,     {.i = +1 } },
+	{ MODKEY|ControlMask,                       XK_d,      incnmaster,     {.i = -1 } },
 	{ MODKEY,                       XK_h,      setmfact,       {.f = -0.05} },
 	{ MODKEY,                       XK_l,      setmfact,       {.f = +0.05} },
-	{ MODKEY,                       XK_Return, zoom,           {0} },
+	{ MODKEY|ShiftMask,                       XK_Return, zoom,           {0} },
 	{ MODKEY,                       XK_Tab,    view,           {0} },
-	{ MODKEY|ShiftMask,             XK_c,      killclient,     {0} },
+	{ MODKEY,             XK_q,      killclient,     {0} },
 	{ MODKEY,                       XK_t,      setlayout,      {.v = &layouts[0]} },
 	{ MODKEY,                       XK_f,      setlayout,      {.v = &layouts[1]} },
 	{ MODKEY,                       XK_m,      setlayout,      {.v = &layouts[2]} },
@@ -94,7 +127,7 @@ static Key keys[] = {
 	TAGKEYS(                        XK_7,                      6)
 	TAGKEYS(                        XK_8,                      7)
 	TAGKEYS(                        XK_9,                      8)
-	{ MODKEY|ShiftMask,             XK_q,      quit,           {0} },
+	{ MODKEY|ShiftMask,             XK_r,      quit,           {0} },
 };
 
 /* button definitions */
